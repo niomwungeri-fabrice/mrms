d32afaec8b433e7f638dec5038d9dc5ff8e79124
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2019-05-18 08:34:30 +0200

    ch(init): add express

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.json({ message: "Hello World!" }));
+
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`));
