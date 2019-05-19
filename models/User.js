8671ae912d30ee2a4d812750133d362801b3e54b
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2019-05-19 07:49:29 +0200

    ch(fix): disable the rule

--- models/User.js
+++ models/User.js
@@ -27,6 +27,7 @@ export default (sequelize, DataTypes) => {
     },
     { tableName: "users" }
   );
+  // eslint-disable-line no-unused-vars
   User.associate = models => {
     // associations can be defined here
   };
