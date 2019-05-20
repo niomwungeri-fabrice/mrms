3652a567b4b15ba82cdfb5dcf28c80f87ac3dfeb
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2019-05-20 13:35:39 +0200

    ft(new): fix hound CI

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -3,16 +3,14 @@ import serverResponses from "./serverResponses";
 const { INTERNAL_SERVER_ERROR } = serverResponses.statusCode;
 const { INTERNAL_SERVER_ERROR_MESSAGE } = serverResponses.errorMessage;
 
-const errorHandler = controller => {
-  return async (req, res, next) => {
-    try {
-      await controller(req, res, next);
-    } catch (error) {
-      return res.status(INTERNAL_SERVER_ERROR).json({
-        message: INTERNAL_SERVER_ERROR_MESSAGE
-      });
-    }
-  };
+const errorHandler = controller => async (req, res, next) => {
+  try {
+    await controller(req, res, next);
+  } catch (error) {
+    return res.status(INTERNAL_SERVER_ERROR).json({
+      message: INTERNAL_SERVER_ERROR_MESSAGE
+    });
+  }
 };
 
 export default errorHandler;
--- server/middlewares/inputValidator.js
+++ server/middlewares/inputValidator.js
@@ -12,17 +12,13 @@ const signUpSchema = Joi.object().keys({
     .min(8)
     .required()
 });
-export const inputValidator = type => {
-  return (req, res, next) => {
-    let validate;
-    if (type === "signup") {
-      validate = Joi.validate(req.body, signUpSchema);
-    }
-    if (validate.error) {
-      return res
-        .status(400)
-        .json({ message: validate.error.details[0].message });
-    }
-    next();
-  };
+export const inputValidator = type => (req, res, next) => {
+  let validate;
+  if (type === "signup") {
+    validate = Joi.validate(req.body, signUpSchema);
+  }
+  if (validate.error) {
+    return res.status(400).json({ message: validate.error.details[0].message });
+  }
+  next();
 };
